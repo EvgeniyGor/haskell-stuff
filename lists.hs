3fcd6189ee0915ed21c3b98ef4080f899527c0a9
Jarith <gorodilov.box@gmail.com>
2017-02-19 15:57:03 +0300

    [Update] lists

--- lists.hs
+++ lists.hs
@@ -7,3 +7,8 @@ sum3 xs ys zs = sum2 zs (sum2 xs ys)
         sum2 xs [] = xs
         sum2 [] ys = ys
         sum2 (x:xs) (y:ys) = (x + y) : sum2 xs ys
+
+
+groupElems :: Eq a => [a] -> [[a]]
+groupElems []     = []
+groupElems xs = let (h, t) = span (== head xs) xs in h : groupElems t  
