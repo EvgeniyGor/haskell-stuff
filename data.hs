360c9cae61e37501750de2fff8b4754c699d17e2
Jarith <gorodilov.box@gmail.com>
2017-02-19 16:30:28 +0300

    [Add] data

---
+++ data.hs
@@ -0,0 +1,25 @@
+module Data where
+
+data Point = Point Double Double
+
+origin :: Point
+origin = Point 0.0 0.0
+
+distanceToOrigin :: Point -> Double
+distanceToOrigin (Point x y) = sqrt (x ^ 2 + y ^ 2)
+
+distance :: Point -> Point -> Double
+distance (Point x1 y1) (Point x2 y2) = sqrt ( (x1 - x2) ^ 2 + (y1 - y2) ^ 2 )
+
+
+
+data Result' = Success' | Fail' Int
+
+instance Show Result' where
+    show Success' = "Success"
+    show (Fail' n) = "Fail: " ++ show n
+
+doSomeWork' :: SomeData -> Result'
+doSomeWork' a | err == 0 = Success'
+              | otherwise = Fail' err
+              where err = snd (doSomeWork a)
